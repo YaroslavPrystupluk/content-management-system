a76a2e8878a31099c34d9b18918277527cad6c3b
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2023-12-20 14:32:59 +0200

    fix

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -2,7 +2,7 @@ import { lazy, Suspense } from 'react';
 
 const Tab = ({ tab }) => {
 	// const MyTabs = lazy(() => import(`/${tab.path}`));
-	const MyTabs = lazy(() => import(`${tab.path}`));
+	const MyTabs = lazy(() => import(`./tabs/${tab.path}.jsx`));
 
 	return (
 		<>
